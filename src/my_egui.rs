7b278990b66e71f1ffcbb0cd94cfef2bdd979ced
John Crawford <john.crawford@crawfishpress.com>
2022-08-13 10:05:35 -0400

    so that's how you resize a Label

--- src/my_egui.rs
+++ src/my_egui.rs
@@ -1,18 +1,18 @@
 /*
-
+Experiments in learning egui.
+https://docs.rs/egui/latest/egui/
 */
 
 use bevy::prelude::*;
-#[allow(unused_imports)]
-use bevy_egui::{egui, EguiContext, EguiPlugin};
-use egui::Color32;
+use bevy_egui::*;
+use egui::*;
 use crate::{GuiData, GameData};
 
 pub fn do_ui_setup(mut egui_context: ResMut<EguiContext>,
                    mut random_data: ResMut<GuiData>,
                    mut move_active: ResMut<GameData>,
 ) {
-    let my_frame = egui::containers::Frame {
+    let my_style_frame = containers::Frame {
         outer_margin: Default::default(),
         inner_margin: egui::style::Margin { left: 10.0, right: 10.0, top: 10.0, bottom: 10.0 },
         rounding: Default::default(),
@@ -23,18 +23,37 @@ pub fn do_ui_setup(mut egui_context: ResMut<EguiContext>,
         stroke: Default::default(),
     };
 
-    egui::TopBottomPanel::top("top panel?").min_height(100.0)
-        .frame(my_frame)
+    let big_text_lbl_1 = RichText::new("This is a Label that is both sized, and Font-color is set.")
+        .color(Color32::WHITE).font(FontId::proportional(20.0));
+
+    let pause_string: String;
+    if move_active.is_paused {
+        pause_string = "Click the magic-button to start".parse().unwrap();
+    } else {
+        pause_string = "Click the magic-button to pause".parse().unwrap();
+    }
+
+    let big_text_btn_1 = RichText::new(pause_string)
+        .color(Color32::WHITE).background_color(Color32::BLACK).font(FontId::proportional(20.0)).size(25.0);
+
+    let big_text_btn_2 = RichText::new("Reset Things")
+        .color(Color32::WHITE).background_color(Color32::BLACK).font(FontId::proportional(20.0)).size(25.0);
+
+    TopBottomPanel::top("top panel?").min_height(80.0)
+        .frame(my_style_frame)
         // .resizable(true)  // Only works if there's a resizable element inside?
         .show(egui_context.ctx_mut(), |ui| {
-            ui.heading("This is a Header that does nothing useful");
-            ui.label("This is just a Label");
-            if ui.button("Click the magic-button to pause").clicked() {
-                random_data.my_value += 1;
-                move_active.is_paused = !move_active.is_paused;
-            }
-            if ui.button("RESET THINGS").clicked() {
-                move_active.game_status = move_active.game_status.cycle();
-            }
+            ui.heading("This is a Header (Label) that does nothing useful");
+            ui.add_sized([420.0, 40.0], Label::new(big_text_lbl_1));
+
+            ui.horizontal(|ui| {
+                if ui.button(big_text_btn_2).clicked() {
+                    move_active.game_status = move_active.game_status.cycle();
+                }
+                if ui.button(big_text_btn_1).clicked() {
+                    random_data.my_value += 1;
+                    move_active.is_paused = !move_active.is_paused;
+                }
+            })
     });
 }
